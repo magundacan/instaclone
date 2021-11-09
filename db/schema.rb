1aea4022d40d3ec8444d59c930e83b4e8d3e3463
abdulmagundacan <83185067+abdulmagundacan@users.noreply.github.com>
2021-11-09 17:58:14 +0800

    Add the necessary view, controller, and route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_075509) do
+ActiveRecord::Schema.define(version: 2021_11_09_093351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_075509) do
   create_table "friend_requests", force: :cascade do |t|
     t.bigint "from_id", null: false
     t.bigint "to_id", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["from_id"], name: "index_friend_requests_on_from_id"
